cd27a48cab3ab7694a9e7719b438e72c3c7b29e3
Ákos Szarvák <metalbogyo@gmail.com>
2022-03-09 20:30:17 +0100

    render board with initial state

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Board.scss";
 import Tile from "../Tile/Tile";
 import Knight from "../Pieces/Knight";
+
+let initialBoard = [];
+initialBoard.push({ piece: "n", x: 0, y: 0 });
+initialBoard.push({ piece: "", x: 0, y: 1 });
+initialBoard.push({ piece: "n", x: 8, y: 1 });
 function Board() {
-  return (
-    <div>
-      <Tile>
-        <Knight></Knight>
-      </Tile>
-    </div>
-  );
+  const [board, setBoard] = useState(initialBoard);
+
+  console.log(board[2].x, board[2].y);
+
+  function renderTile(i, j, board) {
+    let piece = null;
+    board.forEach((b) => {
+      if (b.x === i && b.y === j) {
+        if (b.piece === "n") {
+          piece = <Knight />;
+        } else {
+          piece = null;
+        }
+      }
+    });
+
+    return (
+      <div>
+        <Tile>{piece}</Tile>
+      </div>
+    );
+  }
+
+  const tiles = [];
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      tiles.push(renderTile(i, j, board));
+    }
+  }
+  return <div className="board">{tiles}</div>;
 }
 
 export default Board;
